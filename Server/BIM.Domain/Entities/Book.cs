1611fbcaa51d891f4d1a823325ccb4a656286ab7
arshadahamed <arshadpayoneer@gmail.com>
2024-12-02 23:56:31 +0530

    Defined the book entity

---
+++ Server/BIM.Domain/Entities/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace BIM.Domain.Entities;
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = default!;
+    public string Author { get; set; } = default!;
+    public string Genre { get; set; } = default!;
+    public string ISBN { get; set; } = default!;
+    public DateTime PublishedDate { get; set; }
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+}
