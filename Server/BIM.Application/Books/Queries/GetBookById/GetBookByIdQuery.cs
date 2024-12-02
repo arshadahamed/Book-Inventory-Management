2e04dbcc2bbddf972e3ff89169f252b9f081608b
arshadahamed <arshadpayoneer@gmail.com>
2024-12-03 03:41:12 +0530

    Modified GetBookById Query and  Query Handler with Custom Exception

--- Server/BIM.Application/Books/Queries/GetBookById/GetBookByIdQuery.cs
+++ Server/BIM.Application/Books/Queries/GetBookById/GetBookByIdQuery.cs
@@ -6,4 +6,5 @@ namespace BIM.Application.Books.Queries.GetBookById;
 public class GetBookByIdQuery(int id) : IRequest<BookDto>
 {
     public int Id { get; set; } = id;
+
 }
--- Server/BIM.Application/Books/Queries/GetBookById/GetBookByIdQueryHandler.cs
+++ Server/BIM.Application/Books/Queries/GetBookById/GetBookByIdQueryHandler.cs
@@ -8,23 +8,14 @@ using MediatR;
 
 namespace BIM.Application.Books.Queries.GetBookById;
 
-public class GetBookByIdQueryHandler : IRequestHandler<GetBookByIdQuery, BookDto>
+public class GetBookByIdQueryHandler(IMapper mapper,IBooksRepository booksRepository) : IRequestHandler<GetBookByIdQuery, BookDto>
 {
-    private readonly IMapper _mapper;
-    private readonly IBooksRepository _booksRepository;
-
-    public GetBookByIdQueryHandler(IMapper mapper, IBooksRepository booksRepository)
-    {
-        _mapper = mapper;
-        _booksRepository = booksRepository;
-    }
-
     public async Task<BookDto> Handle(GetBookByIdQuery request, CancellationToken cancellationToken)
     {
-        var book = await _booksRepository.GetByIdAsync(request.Id)
+        var book = await booksRepository.GetByIdAsync(request.Id)
             ?? throw new NotFoundException(nameof(Book), request.Id.ToString());
 
-        var bookDto = _mapper.Map<BookDto>(book);
+        var bookDto = mapper.Map<BookDto>(book);
 
         return bookDto;
     }
