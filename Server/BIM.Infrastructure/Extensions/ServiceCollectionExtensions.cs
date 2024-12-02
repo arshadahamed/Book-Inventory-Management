bbe3167f7410930731c996aab2af3e5ebb9c5722
arshadahamed <arshadpayoneer@gmail.com>
2024-12-03 00:24:33 +0530

    Installed EF and Flexible the Database Connection

---
+++ Server/BIM.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using BIM.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BIM.Infrastructure.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("BIMDb");
+        services.AddDbContext<BIMDbContext>(options =>
+            options.UseSqlServer(connectionString)
+            .EnableSensitiveDataLogging());
+    }
+}
---
+++ Server/BIM.Infrastructure/Persistence/BIMDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BIM.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BIM.Infrastructure.Persistence;
+
+internal class BIMDbContext(DbContextOptions<BIMDbContext> options) : DbContext(options)
+{
+    internal DbSet<Book> Books { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
